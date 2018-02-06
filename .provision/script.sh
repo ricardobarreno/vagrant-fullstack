03d0a593d0d8fffe8f1c1de17b5127e67f13f786
Ricardo Barreno <rickraf.01@gmail.com>
2018-02-05 23:28:52 -0500

    Configuración inicial

---
+++ .provision/script.sh
@@ -0,0 +1,34 @@
+sudo apt-get update
+
+# Instalamos Git
+sudo apt-get install git
+
+
+# Instalamos NodeJS
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+
+
+# Instalamos MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
+
+# Instalamos Vue-cli
+sudo npm install -g vue-cli
+
+# Instalamos Typescript
+sudo npm install -g typescript
+
+# Instalamos nodemon
+sudo npm install -g nodemon
+
+# Instalamos ts-node
+sudo npm install -g ts-node
+
+# Instalación de Mocha
+sudo npm install -g mocha
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# vagrant-fullstack
-Vagrant box fullstack javascript.
+# Vagrant Box Fullstack
+
+This is a Vagrant configuration designed for development with Nodejs, Mongodb and Vue.js (Vue CLI).
+
+
+# Control vagrant
+
+###### Start box
+
+```shell
+vagrant up
+```
+
+
+###### SSH into box
+
+```shell
+vagrant ssh
+```
+
+
+###### Tear down box
+```shell
+vagrant destroy
+```
+
+More documentation on http://www.vagrantup.com/
+
+
+## Control MongoDB
+
+###### Start MongoDB
+
+```shell
+sudo service mongodb start
+```
+
+###### Stop MongoDB
+
+```shell
+sudo service mongodb stop
+```
+
+###### Restart MongoDB
+
+```shell
+sudo service mongodb restart
+```
